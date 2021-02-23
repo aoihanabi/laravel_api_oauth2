e0af4f7485d2895083f47b380b190c66bde63f26
aoihanabi <abii98cm@gmail.com>
2021-02-23 15:17:19 -0600

    Created register method in the new AuthController and implemented a route to access it. Also downgraded cobucci/jwt to solve error with passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Iluminate\Support\Facades\Auth;
+use Validator;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if(!$validator) {
+            return response()->json (
+                ["error"=>$validator->errors()], 422
+            );
+        }
+
+        $input = $request->all();
+        $input["password"] = bcrypt($request->get("password"));
+        $user = User::create($input);
+        $token = $user->createToken("MyApp")->accessToken;
+
+        return response()->json (
+            [
+                "token" => $token,
+                "user" => $user,
+            ], 
+            200
+        );
+    }
+}
