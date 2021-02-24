fb76843e3ac75eb6df0f4904ce2adf3c97b95166
aoihanabi <abii98cm@gmail.com>
2021-02-23 18:57:45 -0600

    Created models and migrations for Actividad Userdata and Confirmacion tables.

---
+++ database/migrations/2021_02_24_003446_create_confirmacion.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfirmacion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirmacion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('iduser');
+            $table->integer('idactividad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('confirmacion', function (Blueprint $table) {
+            Schema::dropIfExists("confirmacion");
+        });
+    }
+}
